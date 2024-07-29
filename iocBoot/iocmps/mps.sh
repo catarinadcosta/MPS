61f95943c186dffa98a8bea1b89770321e9c620c
catarinadcosta <catarina@tifr.res.in>
2024-07-29 17:50:45 +0530

    last updated on 17 July 2024

---
+++ iocBoot/iocmps/mps.sh
@@ -0,0 +1,17 @@
+procServ -n"MPS-QD41XY" -i ^D^C 7011 ./QD41XY.cmd
+procServ -n"MPS-QD1XY" -i ^D^C 7012 ./QD1XY.cmd
+procServ -n"MPS-QD2XY" -i ^D^C 7013 ./QD2XY.cmd
+procServ -n"MPS-QD3XY" -i ^D^C 7014 ./QD3XY.cmd
+procServ -n"MPS-QD4XY" -i ^D^C 7015 ./QD4XY.cmd
+procServ -n"MPS-QD5XY" -i ^D^C 7016 ./QD5XY.cmd
+procServ -n"MPS-QD6XY" -i ^D^C 7017 ./QD6XY.cmd
+procServ -n"MPS-QD7XY" -i ^D^C 7018 ./QD7XY.cmd
+procServ -n"MPS-QD8XY" -i ^D^C 7019 ./QD8XY.cmd
+procServ -n"MPS-QD9XY" -i ^D^C 7020 ./QD9XY.cmd
+procServ -n"MPS-QD10XY" -i ^D^C 7021 ./QD10XY.cmd
+procServ -n"MPS-QD11XY" -i ^D^C 7022 ./QD11XY.cmd
+procServ -n"MPS-QD12XY" -i ^D^C 7023 ./QD12XY.cmd
+procServ -n"MPS-QD13XY" -i ^D^C 7024 ./QD13XY.cmd
+procServ -n"MPS-SWM12" -i ^D^C 7025 ./SWM12.cmd
+procServ -n"MPS-DM14" -i ^D^C 7026 ./DM14.cmd
+procServ -n"MPS-BM12" -i ^D^C 7027 ./BM12.cmd
