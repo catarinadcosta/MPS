4973bce77e05e5b9edbf16772616a324ddd50c28
catarinadcosta <catarina@tifr.res.in>
2025-01-28 16:08:12 +0530

    added upto2025 Jan

--- iocBoot/iocmps/mps.sh
+++ iocBoot/iocmps/mps.sh
@@ -15,3 +15,11 @@ procServ -n"MPS-QD13XY" -i ^D^C 7024 ./QD13XY.cmd
 procServ -n"MPS-SWM12" -i ^D^C 7025 ./SWM12.cmd
 procServ -n"MPS-DM14" -i ^D^C 7026 ./DM14.cmd
 procServ -n"MPS-BM12" -i ^D^C 7027 ./BM12.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS4XY" -i ^D^C 7031 ./MS4XY.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS5XY" -i ^D^C 7032 ./MS5XY.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS7XY" -i ^D^C 7033 ./MS7XY.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS8XY" -i ^D^C 7034 ./MS8XY.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS10XY" -i ^D^C 7035 ./MS10XY.cmd
+procServ -c "/home/linacp206/repos/SteererPS_V1/iocBoot/iocSteererPS_V1" -n"MS-MS11XY" -i ^D^C 7036 ./MS11XY.cmd
+procServ -c "/home/linacp206/repos/Testla_Monitor/iocBoot/ioctestla" -n"TestlaM" -i ^D^C 7029 ./Testla.cmd
+procServ -c "/home/linacp206/repos/BPM/iocBoot/iocBPM" -n"BPM" -i ^D^C 7037 ./bpm.cmd
